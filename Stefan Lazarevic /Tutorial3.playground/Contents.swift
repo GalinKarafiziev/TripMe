343204eca9c6d3c6d6d8044c16a08865e022d0e7
Stefan Lazarevic <s.lazarevic@student.fontys.nl>
2019-02-21 12:58:51 +0100

    Tutorial practice and workshop

--- Stefan Lazarevic /Tutorial3.playground/Contents.swift
+++ Stefan Lazarevic /Tutorial3.playground/Contents.swift
@@ -200,3 +200,171 @@ print(sum5)
 let range1 = 1...10
 let range2=range1
 
+for range2 in range1 {
+    print(pow(Double(range2),2))
+}
+
+for range2 in range1
+{
+    print(sqrt(Double(range2)))
+}
+
+var sum6 = 0
+for row in 0..<8 where row%2==0{
+   
+    for column in 0..<8{
+        sum6 += row * column
+    }
+}
+print(sum6)
+
+let number5 = 10
+
+switch number5 {
+case 0:
+    print("Zero")
+default:
+    print("Non-zero")
+}
+
+let number6 = 10
+
+switch number6 {
+case 10:
+    print("It's ten!")
+default:
+    break
+}
+let string123 = "Dog"
+
+switch string123 {
+case "Cat", "Dog":
+    print("Animal is a house pet.")
+default:
+    print("Animal is not a house pet.")
+}
+
+let hourOfDay2 = 12
+var timeOfDay2: String = ""
+
+switch hourOfDay2 {
+case 0, 1, 2, 3, 4, 5:
+    timeOfDay2 = "Early morning"
+case 6, 7, 8, 9, 10, 11:
+    timeOfDay2 = "Morning"
+case 12, 13, 14, 15, 16:
+    timeOfDay2 = "Afternoon"
+case 17, 18, 19:
+    timeOfDay2 = "Evening"
+case 20, 21, 22, 23:
+    timeOfDay2 = "Late evening"
+default:
+    timeOfDay2 = "INVALID HOUR!"
+}
+
+print(timeOfDay2)
+
+let hourOfDay3 = 17
+var timeOfDay3: String = ""
+
+switch hourOfDay3 {
+case 0...5:
+    timeOfDay3 = "Early morning"
+case 6...11:
+    timeOfDay3 = "Morning"
+case 12...16:
+    timeOfDay3 = "Afternoon"
+case 17...19:
+    timeOfDay3 = "Evening"
+case 20..<24:
+    timeOfDay3 = "Late evening"
+default:
+    timeOfDay3 = "INVALID HOUR!"
+}
+print(timeOfDay3)
+
+let number8 = 7
+
+switch number8 {
+case let x where x % 2 == 0:
+    print("Even")
+default:
+    print("Odd")
+}
+
+let coordinates = (x: 3, y: 2, z: 5)
+
+switch coordinates {
+case (0, 0, 0): // 1
+    print("Origin")
+case (_, 0, 0): // 2
+    print("On the x-axis.")
+case (0, _, 0): // 3
+    print("On the y-axis.")
+case (0, 0, _): // 4
+    print("On the z-axis.")
+default:        // 5
+    print("Somewhere in space")
+}
+let coordinates2 = (x: 3, y: 2, z: 5)
+
+switch coordinates2 {
+case (0, 0, 0):
+    print("Origin")
+case (let x, 0, 0):
+    print("On the x-axis at x = \(x)")
+case (0, let y, 0):
+    print("On the y-axis at y = \(y)")
+case (0, 0, let z):
+    print("On the z-axis at z = \(z)")
+case let (x, y, z):
+    print("Somewhere in space at x = \(x), y = \(y), z = \(z)")
+}
+let coordinates3 = (x: 3, y: 9, z: 5)
+
+switch coordinates3 {
+case let (x, y, _) where y == x:
+    print("Along the y = x line.")
+case let (x, y, _) where y == x * x:
+    print("Along the y = x^2 line.")
+default:
+    break
+}
+
+let age4 = 20
+
+switch age4{
+case 0...2:
+    print("Infant")
+case 3...12:
+    print("Child")
+case 13...19:
+    print("Teenager")
+case 20...39:
+    print("Adult")
+case 40...60:
+    print("Middle aged")
+default:
+    print("Elderly")
+}
+
+switch coordinates {
+case let (x, y, z) where x == y && y == z:
+    print("x = y = z")
+case (_, _, 0):
+    print("On the x/y plane")
+case (_, 0, _):
+    print("On the x/z plane")
+case (0, _, _):
+    print("On the y/z plane")
+default:
+    print("Nothing special")
+}
+ var temp = 10
+for _ in 0...10
+{
+    
+    print(temp)
+    temp=temp-1
+}
+
